f7189def6cf64c70703de6387cbc1b854307e34b
yazayalks <lysak_kirill@mail.ru>
2022-10-01 19:06:28 +0700

    Add burger-menu

--- src/App.js
+++ src/App.js
@@ -2,36 +2,38 @@
 import {Component} from "react";
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd";
+import {HashRouter, NavLink, Route, Routes} from "react-router-dom";
 
 const date1 = new Date(2021, 7, 19, 14,5);
 const date2 = new Date(2021, 7, 19, 15, 23);
 
 const initialData = [
   {
-   title: 'Изучить React',
-   desc: 'В срочном порядке!',
-   image: '',
-    done: true,
-    createAt: date1.toLocaleString(),
-    key: date1.getTime()
+      title: 'Изучить React',
+      desc: 'В срочном порядке!',
+      image: '',
+      done: true,
+      createAt: date1.toLocaleString(),
+      key: date1.getTime()
   },
   {
-    title: 'Написать React-приложение',
-    desc: 'Список запланированных дел',
-    image: '',
-    done: false,
-    createAt: date2.toLocaleString(),
-    key: date2.getTime()
+      title: 'Написать React-приложение',
+      desc: 'Список запланированных дел',
+      image: '',
+      done: false,
+      createAt: date2.toLocaleString(),
+      key: date2.getTime()
   }
 ];
 
 export default class App extends Component{
   constructor(props) {
     super(props);
-    this.state = {data: initialData};
+    this.state = {data: initialData, showMenu: false};
     this.setDone = this.setDone.bind(this);
     this.delete = this.delete.bind(this);
-    this.add = this.add.bind(this)
+    this.add = this.add.bind(this);
+    this.showMenu = this.showMenu.bind(this);
   }
   setDone(key) {
       const deed = this.state.data.find((current) => current.key === key);
@@ -50,25 +52,52 @@ export default class App extends Component{
       this.state.data.push(deed);
       this.setState((state) => ({}));
   }
+  showMenu(evt) {
+      evt.preventDefault();
+      this.setState((state) => ({ showMenu: !state.showMenu}));
+  }
   render() {
     return(
-        <div>
+        <HashRouter>
           <nav className="navbar is-light">
             <div className="navbar-brand">
-              <span className="navbar-item is-uppercase">
-                Todos
-              </span>
+              <NavLink to='/'
+                       className={({isActive}) => 'navbar-item is-uppercase' + (isActive ? ' is-active' : '')
+              }>
+                  Todos
+              </NavLink>
+                <a href='/'
+                   className={this.state.showMenu ? 'navbar-burger is-active' : 'navbar-burger'}
+                   onClick={this.showMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+
             </div>
+              <div className={this.state.showMenu ? 'navbar-menu is-active' : 'navbar-menu'}
+                   onClick={this.showMenu}>
+                  <div className="navbar-start">
+                      <NavLink to='/add'
+                               className={({isActive}) => 'navbar-item' + (isActive ? ' is-active' : '')}>
+                          Создать дело
+                      </NavLink>
+                  </div>
+              </div>
           </nav>
           <main className="content px-6 mt-6">
-            <TodoList list = {this.state.data}
-                      // setDone = {this.setDone}
-                      // delete = {this.delete}
-            />
-              <TodoAdd add={this.add}
-              />
+              <Routes>
+                  <Route path = '/' element={
+                      <TodoList list = {this.state.data}
+                                setDone = {this.setDone}
+                                delete = {this.delete}/>
+                  }/>
+                  <Route path = '/add' element={
+                      <TodoAdd add={this.add}/>
+                  }/>
+              </Routes>
           </main>
-        </div>
+        </HashRouter>
     );
   }
 }
