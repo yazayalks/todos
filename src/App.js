410fec4a565d619b7e414f5496321408f7463b62
yazayalks <lysak_kirill@mail.ru>
2022-10-01 16:11:53 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from "react";
+import TodoList from "./TodoList";
+
+const date1 = new Date(2021, 7, 19, 14,5);
+const date2 = new Date(2021, 7, 19, 15, 23);
+
+const initialData = [
+  {
+   title: 'Изучить React',
+   desc: 'В срочном порядке!',
+   image: '',
+    done: true,
+    createAt: date1.toLocaleString(),
+    key: date1.getTime()
+  },
+  {
+    title: 'Написать React-приложение',
+    desc: 'Список запланированных дел',
+    image: '',
+    done: false,
+    createAt: date2.toLocaleString(),
+    key: date2.getTime()
+  }
+];
+
+export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {data: initialData};
+    this.setDone = this.setDone.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+  setDone(key) {
+      const deed = this.state.data.find((current) => current.key === key);
+      if (deed) {
+          deed.done = true;
+          this.setState((state) => ({}));
+      }
+  }
+  delete(key) {
+      const newData = this.state.data.filter(
+          (current) => current.key !== key
+      );
+      this.setState((state) => ({data: newData}));
+  }
+  render() {
+    return(
+        <div>
+          <nav className="navbar is-light">
+            <div className="navbar-brand">
+              <span className="navbar-item is-uppercase">
+                Todos
+              </span>
+            </div>
+          </nav>
+          <main className="content px-6 mt-6">
+            <TodoList list = {this.state.data}
+                      setDone = {this.setDone}
+                      delete = {this.delete}/>
+          </main>
+        </div>
+    );
+  }
 }
-
-export default App;
