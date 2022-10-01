bd5e16367c20c543774ea0e2a821f61df4bf29f0
yazayalks <lysak_kirill@mail.ru>
2022-10-01 23:08:04 +0700

    connect database firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCV-GNcEsix4pLAO7ewtS2LDBfBAGzUBuQ",
+    authDomain: "todos-126de.firebaseapp.com",
+    projectId: "todos-126de",
+    storageBucket: "todos-126de.appspot.com",
+    messagingSenderId: "172094231766",
+    appId: "1:172094231766:web:18be193d70407b4bc1a577",
+    databaseURL: "https://todos-126de-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const firebaseAConfig = initializeApp(firebaseConfig);
+export default firebaseAConfig;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
